ab4720035d12dd3ffd86949f07b278e272dc71d3
fisalm29 <faisal.muhammad2911@gmail.com>
2023-10-01 19:13:47 +0700

    accordion component is done

--- src/components/AccordionItem.tsx
+++ src/components/AccordionItem.tsx
@@ -14,10 +14,22 @@ const AccordionItem = ({
   btnOnClick,
 }: AccordionItemProps): JSX.Element => {
   const [visibility, setVisibility] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState(0);
 
   const togleVisibility = () => {
     setVisibility((v) => !v);
   };
+
+  useEffect(() => {
+    if (visibility) {
+      const contentEl = contentRef.current as HTMLDivElement;
+
+      setHeight(contentEl.scrollHeight);
+    } else {
+      setHeight(0);
+    }
+  }, [visibility]);
 
   return (
     <li>
@@ -25,17 +37,24 @@ const AccordionItem = ({
         className="flex w-full items-center justify-between bg-surface px-6 py-3"
         onClick={togleVisibility}
       >
-        <span>{item.label}</span>
         <span>
+          <p className="font-bold">{item.label}</p>
+        </span>
+        <span
+          className={`transform duration-200 ease-in-out ${
+            visibility ? "-rotate-180" : ""
+          }`}
+        >
           <Dropdown width={16} height={16} fill="#252525" />
         </span>
       </button>
       <div
-        className={`transition-height overflow-hidden bg-surface duration-200 ease-in-out ${
-          visibility ? "block" : "hidden"
-        }`}
+        className="transition-height overflow-hidden bg-surface duration-200 ease-in-out"
+        style={{ height }}
       >
-        <p>{item.description}</p>
+        <p ref={contentRef} className="p-4">
+          {item.description}
+        </p>
       </div>
     </li>
   );
